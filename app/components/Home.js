d2f3913fcdca547745bd4b169de9d3f496ccbc43
Ariel Zusman <ariel.zusman@gmail.com>
2016-05-26 16:27:12 +0300

    js standard-format

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,21 +1,24 @@
 var React = require('react'),
-	transparentBg = require('../styles').transparentBg,
-	ReactRouter = require('react-router'),
-	Link = ReactRouter.Link
-
+  transparentBg = require('../styles').transparentBg,
+  ReactRouter = require('react-router'),
+  Link = ReactRouter.Link
 
 var Home = React.createClass({
-	render: function(){
-		return(
-			<div className="jumbotron col-sm-12 text-center" styles={transparentBg}>
-				<h1>Github Battle</h1>
-				<p className="lead">Some fanby motto</p>
-				<Link to="/playerOne">
-					<button type='button' className='btn btn-lg btn-success'>Get Started</button>
-				</Link>
-			</div>
-		)
-	}
+  render: function () {
+    return (
+    <div className="jumbotron col-sm-12 text-center" styles={transparentBg}>
+      <h1>Github Battle</h1>
+      <p className="lead">
+        Some fanby motto
+      </p>
+      <Link to="/playerOne">
+      <button type='button' className='btn btn-lg btn-success'>
+        Get Started
+      </button>
+      </Link>
+    </div>
+    )
+  }
 })
 
 module.exports = Home
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,14 +1,14 @@
 var React = require('react')
 
 var Main = React.createClass({
-	render: function(){
-		return(
-			<div className='main-container'>
-			 	{this.props.children}
-		 	</div>
+  render: function () {
+    return (
+    <div className='main-container'>
+      {this.props.children}
+    </div>
 
-		)
-	}
+    )
+  }
 })
 
 module.exports = Main
