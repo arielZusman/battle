6d71dcd055c1120f17b2be315445506ac0ad8777
unknown <arielz@dev-arialz.traderxp.local>
2016-05-30 07:45:35 +0300

    video 5 done

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -1,15 +1,17 @@
 var React = require('react'),
+    PropTypes = React.PropTypes,
     transparentBg = require('../styles').transparentBg
 
-var Prompt = React.createClass({
-  render: function(){
-    return (
+//functional stateless components - used when component is only has ui and proptypes
+// instead of creating a class
+function Prompt(props) {
+  return (
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-      <h1>{this.props.header}</h1>
+      <h1>{props.header}</h1>
       <div className="col-sm-12">
-        <form onSubmit={this.props.onSubmitUser}>
+        <form onSubmit={props.onSubmitUser}>
           <div className="form-group">
-            <input className="form-control" placeholder="Github Username" onChange={this.props.onUpdateUser} value={this.props.username} type="text" />
+            <input className="form-control" placeholder="Github Username" onChange={props.onUpdateUser} value={props.username} type="text" />
           </div>
           <div className="form-group col-sm-4 col-sm-offset-4">
             <button className="btn btn-block btn-success" type="submit">
@@ -19,9 +21,14 @@ var Prompt = React.createClass({
         </form>
       </div>
     </div>      
-    )
-  }
-})
+    );
+}
+
+Prompt.propTypes = {
+  header: PropTypes.string.isRequired,
+  onUpdateUser: PropTypes.func.isRequired,
+  onSubmitUser: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired
+}
 
 module.exports = Prompt
-
