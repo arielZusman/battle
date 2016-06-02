c05bc48801ae1812845b86514c22b476d822fb2d
Ariel Zusman <ariel.zusman@gmail.com>
2016-06-02 09:56:17 +0300

    refactoring to es6

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,12 +1,11 @@
 var React = require('react')
 
-function puke(obj) {
-	return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-}
-function ConfirmBattle(props){
-	return props.isLoading === true 
+const puke = obj => <pre>{JSON.stringify(obj, null, ' ')}</pre>
+
+const ConfirmBattle = props => 
+		props.isLoading === true 
 		? <p> Loading </p>
 		: <div> Confirm Battle: {puke(props)} </div>	
-}
+
 
 module.exports = ConfirmBattle
